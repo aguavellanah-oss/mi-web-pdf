a9f8fd2c1d51d8382c803c71db36fc57add9ca76
aguavellanah-oss <aguavellanah@gmail.com>
2026-03-09 11:18:29 -0300

    fix render port

--- mi_programa/app.py
+++ mi_programa/app.py
@@ -19,5 +19,9 @@ def crear_pdf():
     buffer.seek(0)
     return send_file(buffer, as_attachment=True, download_name="archivo.pdf")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
